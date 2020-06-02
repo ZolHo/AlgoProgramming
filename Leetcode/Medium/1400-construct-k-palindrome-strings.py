ecd34120bdf0fe36b01b947d178254446755cb98
hao <cc.lancelot9@gmail.com>
2020-06-02 21:45:22 +0800

    1004 优化

--- Leetcode/Medium/1400-construct-k-palindrome-strings.py
+++ Leetcode/Medium/1400-construct-k-palindrome-strings.py
@@ -4,8 +4,9 @@ class Solution:
             return False
 
         chart = [0 for i in range(36)]
+        theA = ord('a')
         for c in s:
-            chart[ord(c)-ord('a')] += 1
+            chart[ord(c)-theA] += 1
         orr, mul = 0, 0
         for i in chart:
             if (i%2==0 and i!=0):
@@ -14,9 +15,9 @@ class Solution:
                 orr += 1
                 mul += i//2
 
-        for i in chart:
-            print(i,end=" ")
-        print(orr,mul)
+        # for i in chart:
+        #     print(i,end=" ")
+        # print(orr,mul)
         if (orr > k) :
             return False
         elif (k-orr>2*mul):
@@ -24,7 +25,7 @@ class Solution:
         else:
             return True
 
-#执行用时 :244 ms, 在所有 Python3 提交中击败了8.61%的用户
+#执行用时 :156 ms, 在所有 Python3 提交中击败了28.92%的用户
 #内存消耗 :14.6 MB, 在所有 Python3 提交中击败了100.00%的用户
 
 solu = Solution()
