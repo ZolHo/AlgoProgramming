fedf4a39b75fc5b12309a34db0af8d802ca12329
hao <cc.lancelot9@gmail.com>
2020-06-09 09:22:13 +0800

    MS46 by DP

--- Leetcode/Medium/MS46-ba-shu-zi-fan-yi-cheng-zi-fu-chuan-lcof.py
+++ Leetcode/Medium/MS46-ba-shu-zi-fan-yi-cheng-zi-fu-chuan-lcof.py
@@ -5,21 +5,37 @@
 
 class Solution:
     def translateNum(self, num: int) -> int:
-        ans, s = 0, str(num)
-        def dfs(i):
-            if (i>= len(s)):
-                nonlocal ans
-                ans += 1
-                return
+        ## dp
 
-            dfs(i+1)
-            if (i+1<len(s) and s[i]!='0' and int(s[i:i+2])<=25):
-                dfs(i+2)
-        dfs(0)
-        return ans
+        s = str(num)
+        dp = [0 for x in range(0,len(s)+1)]
+        dp[0], dp[1] = 1, 1
+        for i in range(1,len(s)):
+            print(s[i:i+2])
+            if (i+1<=len(s) and s[i-1]!='0' and int(s[i-1:i+1])<=25) :
+                dp[i+1] = dp[i]+dp[i-1]
+            else:
+                dp[i+1] = dp[i]
+        # print (dp)
+        return dp[len(s)]
 
-#执行用时 :40 ms, 在所有 Python3 提交中击败了64.02%的用户
+        ## DFS
+
+        # ans, s = 0, str(num)
+        # def dfs(i):
+        #     if (i>= len(s)):
+        #         nonlocal ans
+        #         ans += 1
+        #         return
+        #
+        #     dfs(i+1)
+        #     if (i+1<len(s) and s[i]!='0' and int(s[i:i+2])<=25):
+        #         dfs(i+2)
+        # dfs(0)
+        # return ans
+
+#执行用时 :36 ms, 在所有 Python3 提交中击败了83.43%的用户
 #内存消耗 :13.8 MB, 在所有 Python3 提交中击败了100.00%
 
 solu = Solution()
-print(solu.translateNum(506))
+print(solu.translateNum(25))
