365bbc5bdd2243a75e87a8965ac16644092d0ce0
hao <cc.lancelot9@gmail.com>
2020-07-08 20:46:47 +0800

    区间加

--- template/树状数组.py
+++ template/树状数组.py
@@ -18,6 +18,7 @@ class bitTree():
         while i <= self.n :
             self.a[i] += val;
             i += self.lowbit(i);
+
     def sum (self, i) :
         i = i+1
         ret = 0;
@@ -26,5 +27,37 @@ class bitTree():
             i -= self.lowbit(i);
         return ret;
 
+class bitTreeQJXG():
+    def __init__(self, arr, n) :
+        self.n = n
+        self.a = [0 for i in range(n+1)]
+        self.update(0,arr[0])
+        for i in range(1,n):
+            self.update(i, arr[i]-arr[i-1])
+
+    def lowbit(self, i) :
+        return i & -i;
+
+    def update (self, i,  val) :
+        i = i+1
+        while i <= self.n:
+            self.a[i] += val
+            i += self.lowbit(i)
+
+    def range_update(self,l,r,val):
+        self.update(l,val)
+        self.update(r+1, -val)
+
+    def get (self, i) :
+        i = i+1
+        ret = 0;
+        while i > 0 :
+            ret += self.a[i];
+            i -= self.lowbit(i);
+        return ret;
+
 bt = bitTree([1,2,3,2,1,2,3],7)
-print(bt.sum(6))
+# print(bt.sum(6))
+bs = bitTreeQJXG([1,2,3,4,5,6,7,8],8)
+bs.range_update(0,6,100)
+print(bs.get(0),bs.get(7))
